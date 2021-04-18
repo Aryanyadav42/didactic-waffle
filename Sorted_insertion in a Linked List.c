f02e1baa094ca3889c505d4cd26a559869bc7ba3
Aryanyadav42 <82714600+Aryanyadav42@users.noreply.github.com>
2021-04-18 17:39:25 +0530

    Add files via upload

---
+++ Sorted_insertion in a Linked List.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node{
+   int data;
+   struct node *next;
+}*first=0,*last=0;
+
+void Sorted_Insertion(int *data){
+    struct node *p,*q,*t;
+    p = first;
+
+    t = (struct node *)malloc(sizeof(struct node));
+    if(t==NULL){
+        printf("\nHeap is Full");
+        // As all the data is stored in heap dynamically
+    }
+    else{
+        t->data = *data;
+        if(*data<=first->data){
+            t->next = first;
+            first = t;
+        }
+        else if(*data >= last->data){
+            t->next = NULL;
+            last->next = t;
+            last = t;
+        }
+        else{
+            q=0;
+            while(p->data<=*data && p){
+                q = p;
+                p = p->next;
+            }
+            if(p){
+                t->next = q->next;
+                q->next = t;
+            }
+        }
+    }
+}
+
+int main(){
+    void Sorted_Insertion();
+    struct node *t,*p;
+    int i,count=0;
+
+    first = (struct node *)malloc(sizeof(struct node));
+    first->data = NULL;
+    first->next = NULL;
+    last = first;
+
+    do{
+        t = (struct node *)malloc(sizeof(struct node));
+
+        if(t==NULL){
+            printf("\nHeap is Full");
+            break;
+        }
+        else{
+            scanf("%d",&t->data);
+            if(first->data==NULL && count==0 && (t->data!=0 || t->data==0))
+                first=last=t;
+            else{
+                t->next = NULL;
+                last->next=t;
+                last=t;
+            }
+        }
+        count++;
+        scanf("%d",&i);
+    }while(i);
+
+    int data;
+    printf("\nEnter the data you want to insert : ");
+    scanf("%d",&data);
+
+    Sorted_Insertion(&data);
+
+    p = first;
+    while(p!=NULL){
+        printf("\n%d",p->data);
+        p = p->next;
+    }
+}
+
+
